c5384fef6c7e751ed7c667716ad74032a95d0089
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-10-18 20:32:02 +0500

    Task3

---
+++ Tyuiu.SbrodovSV.Sprint3.Task3.V28/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SbrodovSV.Sprint3.Task3.V28.Lib;
+
+namespace Tyuiu.SbrodovSV.Sprint3.Task3.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Сбродов С. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы цикличекой структуры                                    *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Сбродов Степан Владимирович | АСОиУб-23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить цифры на букву r в строке:              *");
+            Console.WriteLine("* f35hyt t4j 3gkg45                                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string str = "f35hyt t4j 3gkg45";
+            char chr = 'r';
+
+            Console.WriteLine($"Исходная строка: {str}");
+            Console.WriteLine($"Искомый символ: {chr}");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write($"Результат = {dataService.ReplaceNumOnChar(str,chr)}");
+
+            Console.ReadKey();
+        }
+    }
+}
