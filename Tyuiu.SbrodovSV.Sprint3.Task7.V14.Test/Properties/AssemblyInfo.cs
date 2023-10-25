d882f4287d51fe3c1660971ffa1d27312fb60d4f
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-10-25 22:41:06 +0500

    Task7

---
+++ Tyuiu.SbrodovSV.Sprint3.Task7.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SbrodovSV.Sprint3.Task7.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SbrodovSV.Sprint3.Task7.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd8b4e0f-c2dd-4ad9-9675-41a13e693062")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SbrodovSV.Sprint3.Task7.V14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SbrodovSV.Sprint3.Task7.V14.Lib;
+
+namespace Tyuiu.SbrodovSV.Sprint3.Task7.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Сбродов С. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы цикличекой структуры                                    *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Сбродов Степан Владимирович | АСОиУб-23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [19, 30] сумму всех делителей                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5, stopValue = 5;
+            double[] res = dataService.GetMassFunction(startValue,stopValue);
+
+            Console.WriteLine($"Начало отрезка: {startValue}");
+            Console.WriteLine($"Конец отрезка: {stopValue}");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+
+            for (int i = 0; i < res.Length; i++, startValue++)
+            {
+                Console.WriteLine("|{0,5:d}     | {1,6:f2}    |", startValue, res[i]);
+            }
+
+            Console.WriteLine("+----------+-----------+");
+
+            Console.ReadKey();
+        }
+    }
+}
